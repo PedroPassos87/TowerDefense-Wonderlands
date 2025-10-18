4eb05e1c28d6b58439addc40141ec0a2e855632c
PedroPassos87 <pedro.passos@inatel.br>
2025-10-18 18:00:20 -0300

    feat: enemy scriptable object

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -2,35 +2,37 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+
+    [SerializeField] private EnemyData data;
     
     [SerializeField] private float moveSpeed = 3f;
-    [SerializeField] private Path currentPath;
+    private Path _currentPath;
 
     private Vector3 _targetPosition;
     private int _currentWaypoint;
 
     private void Awake()
     {
-        currentPath = GameObject.Find("Path1").GetComponent<Path>();
+        _currentPath = GameObject.Find("Path1").GetComponent<Path>();
     }
 
     private void OnEnable()
     {
         _currentWaypoint = 0;
-        _targetPosition = currentPath.GetPosition(_currentWaypoint);
+        _targetPosition = _currentPath.GetPosition(_currentWaypoint);
     }
 
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, _targetPosition, moveSpeed * Time.deltaTime);    
+        transform.position = Vector3.MoveTowards(transform.position, _targetPosition, data.speed * Time.deltaTime);    
         float relativeDistance = (transform.position - _targetPosition).magnitude;
 
         if(relativeDistance < 0.1f)
         {
-            if(_currentWaypoint < currentPath.Waypoints.Length - 1)
+            if(_currentWaypoint < _currentPath.Waypoints.Length - 1)
             {
                 _currentWaypoint++;
-                _targetPosition = currentPath.GetPosition(_currentWaypoint);
+                _targetPosition = _currentPath.GetPosition(_currentWaypoint);
             }
             else
             {
